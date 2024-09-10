eea4e000bbff800efb7c620a7c4464cb95ca0ea6
Michal Zdziennicki <m.zdziennick@student.uw.edu.pl>
2024-09-10 21:50:32 +0200

    Poprawione kolory

--- plots.py
+++ plots.py
@@ -1,24 +1,50 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import matplotlib.colors
 import numpy as np
 
 
 # Read the CSV file
-data = pd.read_csv('Spectral_02.csv', header=None)
+data = pd.read_csv('Spectral_colored.csv', header=None)
 data1 = pd.read_csv('Scattered_01.csv', header=None)
 data1 = data1.sort_values(by=0)
+data2 = pd.read_csv('Before_scat_colored.csv', header=None)
 
 # Data extraction
 x = data[0]
 y = data[1]
+color = data[2]
 x1 = data1[0]
 y1 = data1[1]
+x2 = data2[0]
+y2 = data2[1]
+color2 = data2[2]
 
-# Plot sections
 
+#Before scattering code
+cm = plt.cm.jet
+norm = matplotlib.colors.Normalize(vmin=min(color), vmax=max(color))
+sc=plt.scatter(x2, y2, marker='s', c=color2, vmin=min(color2), vmax=max(color2), cmap=cm, s=12, edgecolors='0.3', linewidths=0.5)
+plt.gca().spines['top'].set_linewidth(2)  
+plt.gca().spines['bottom'].set_linewidth(2)  
+plt.gca().spines['left'].set_linewidth(2)  
+plt.gca().spines['right'].set_linewidth(2) 
+plt.grid(True, alpha=0.1)
+plt.xlim(-100, 100)  # Set the range of the x-axis
+plt.ylim(0, 20)  # Set the range of the x-axis
+plt.xlabel('$R_{vdW}/a_{1D}$')
+plt.ylabel('$E/E_{\omega}$')
+plt.title('3D Bound state energy')
+plt.colorbar(sc)
+plt.savefig('3D_Bound_state_energy_before_scat.png')
+
+#clear plot
+plt.clf()
 
 # Bound state energy
-plt.scatter(x, y, marker='s', color='C0', s=12, edgecolors='0.3', linewidths=0.5)
+cm = plt.cm.jet
+norm = matplotlib.colors.Normalize(vmin=min(color), vmax=max(color))
+sc=plt.scatter(x, y, marker='s', c=color, vmin=min(color), vmax=max(color), cmap=cm, s=5, edgecolors='0.3', linewidths=0.5)
 plt.gca().spines['top'].set_linewidth(2)  
 plt.gca().spines['bottom'].set_linewidth(2)  
 plt.gca().spines['left'].set_linewidth(2)  
@@ -29,6 +55,7 @@ plt.ylim(0, 20)  # Set the range of the x-axis
 plt.xlabel('$R_{vdW}/a_{3D}$')
 plt.ylabel('$E/E_{\omega}$')
 plt.title('3D Bound state energy')
+plt.colorbar(sc)
 plt.savefig('3D_Bound_state_energy.png')
 
 #clear plot
